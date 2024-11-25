dc24d2de1a193a8b4bd7cb8a6274e31b76f86574
LuckyDog <lucki@vip.qq.com>
2024-11-25 13:15:07 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -10,13 +10,6 @@ export default function Footer() {
         <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("p_146-598_Findthecodeon")}{" "}
           <a
-            href="https://github.com/hamster1963/nezha-dash"
-            target="_blank"
-            className="cursor-pointer font-normal underline decoration-yellow-500 hover:decoration-yellow-300 transition-colors decoration-2 underline-offset-2 dark:decoration-yellow-900 dark:hover:decoration-yellow-600"
-          >
-            {t("a_303-585_GitHub")}
-          </a>
-          <a
             href={`https://github.com/hamster1963/nezha-dash/releases/tag/v${version}`}
             target="_blank"
             className="cursor-pointer font-normal underline decoration-yellow-500 hover:decoration-yellow-300 transition-colors decoration-2 underline-offset-2 dark:decoration-yellow-900 dark:hover:decoration-yellow-600"
@@ -26,7 +19,6 @@ export default function Footer() {
         </p>
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("section_607-869_2020")}
-          {new Date().getFullYear()}{" "}
           <a href={"https://blog.iotfen.com/"}>{t("a_800-850_Hamster1963")}</a>
         </section>
       </section>
