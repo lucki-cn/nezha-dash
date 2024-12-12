f11da04dfc32a088d63eb4e03ab7ffc8236d8026
LuckyDog <lucki@vip.qq.com>
2024-12-12 19:29:38 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
           </p>
         </p>
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
-          {t("section_607-869_2020")}
+          {t("section_607-869_2020")}{t("a_800-850_Hamster1963")}
         </section>
       </section>
     </footer>
